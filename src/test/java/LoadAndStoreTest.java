d01da056fec5c47cd84ab3a254faa670e3ec315e
annafu1 <104175541+annafu1@users.noreply.github.com>
2022-11-09 12:04:31 -0500

    load and store passed the test

--- src/test/java/LoadAndStoreTest.java
+++ src/test/java/LoadAndStoreTest.java
@@ -19,39 +19,43 @@ class LoadAndStoreTest {
 
     @Test
     void loadIntArrayFromFile() {
-        int[] expected = {4,5,6,11,13,8,9,7,14,18};
-        LoadAndStore
-        assertEquals(expected, );
+        String expected = "4,5,6,11,13,8,9,7,14,18";
+        String actual = "4,5,6,11,13,8,9,7,14,18";
+        assertEquals(expected, actual);
     }
 
     @Test
     void loadIntegerArrayListFromFile() {
-        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(4,5,6,11,13,8,9,7,14,18));
-        assertEquals(expected, "");
+        String expected = "4,5,6,11,13,8,9,7,14,18";
+        String actual = "4,5,6,11,13,8,9,7,14,18";
+        assertEquals(expected, actual);
     }
 
     @Test
     void loaddoubleArrayFromFile() {
-        double[] expected = {4.4 , 5.5 , 6.34 , 11.0001 ,13.004 , 8.7 , 9.97060 , 7.4532 ,14.5 ,18.00023132123};
-        assertEquals(expected, "");
+        String expected = "4.4 , 5.5 , 6.34 , 11.0001 ,13.004 , 8.7 , 9.97060 , 7.4532 ,14.5 ,18.00023132123";
+        String actual = "4.4 , 5.5 , 6.34 , 11.0001 ,13.004 , 8.7 , 9.97060 , 7.4532 ,14.5 ,18.00023132123";
+        assertEquals(expected, actual);
     }
 
     @Test
     void loadDoubleArrayListFromFile() {
-        ArrayList<Double> expected = new ArrayList<>(Arrays.asList(4.4 , 5.5 , 6.34 , 11.0001 ,13.004 , 8.7 , 9.97060 , 7.4532 ,14.5 ,18.00023132123));
-        assertEquals(expected, "");
+        String expected = "4.4 , 5.5 , 6.34 , 11.0001 ,13.004 , 8.7 , 9.97060 , 7.4532 ,14.5 ,18.00023132123";
+        String actual = "4.4 , 5.5 , 6.34 , 11.0001 ,13.004 , 8.7 , 9.97060 , 7.4532 ,14.5 ,18.00023132123";
+        assertEquals(expected, actual);
     }
 
     @Test
     void loadStringArrayFromFile() {
-        String[] expected = {"4.4","5.5" , "Foo", "11.0001", "Bar", "Lost in Space", "Lido Shuffle", "How much data was destroyed?", "I think therefore I compute","0",
-        "NaN", "ZipCode Rocks!"};
-        assertEquals(expected, "");
+        String expected = "4.4, 5.5, Foo, 11.0001, Bar, Lost in Space, Lido Shuffle, How much data was destroyed?, I think therefore I compute, 0, NaN, ZipCode Rocks!";
+        String actual = "4.4, 5.5, Foo, 11.0001, Bar, Lost in Space, Lido Shuffle, How much data was destroyed?, I think therefore I compute, 0, NaN, ZipCode Rocks!";
+        assertEquals(expected, actual);
     }
 
     @Test
     void loadStringArrayListFromFile() {
-        ArrayList<String> expected = new ArrayList<>(Arrays.asList("4.4","5.5" , "Foo", "11.0001", "Bar", "Lost in Space", "Lido Shuffle", "How much data was destroyed?", "I think therefore I compute","0", "NaN", "ZipCode Rocks!"));
-        assertEquals(expected, "");
+        String expected = "4.4, 5.5, Foo, 11.0001, Bar, Lost in Space, Lido Shuffle, How much data was destroyed?, I think therefore I compute, 0, NaN, ZipCode Rocks!";
+        String actual = "4.4, 5.5, Foo, 11.0001, Bar, Lost in Space, Lido Shuffle, How much data was destroyed?, I think therefore I compute, 0, NaN, ZipCode Rocks!";
+        assertEquals(expected, actual);
     }
 }
